4347bd8e533b8065b7eb48c697cfd3852186f17a
gongjuheon <yh6543210@gmail.com>
2025-05-10 01:39:58 +0900

    init(be): initial commit of backend

---
+++ src/main/java/com/lucvs/tempesta/TempestaApplication.java
@@ -0,0 +1,13 @@
+package com.lucvs.tempesta;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TempestaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TempestaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lucvs/tempesta/TempestaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lucvs.tempesta;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TempestaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
