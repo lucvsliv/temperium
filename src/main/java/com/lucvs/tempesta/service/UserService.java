11856111b0fdfe8e94a7c613aa3324d4c8bccf94
gongjuheon <yh6543210@gmail.com>
2025-05-14 00:33:02 +0900

    feat(be): add User entity class service interface

--- src/main/java/com/lucvs/tempesta/service/UserService.java
+++ src/main/java/com/lucvs/tempesta/service/UserService.java
@@ -1,4 +1,14 @@
 package com.lucvs.tempesta.service;
 
+import com.lucvs.tempesta.entity.User;
+
+import java.util.List;
+
 public interface UserService {
+
+    User createUser(User user);
+    User getUserById(Long userId);
+    User getUserByUsername(String username);
+    List<User> getAllUsers();
+    void deleteUser(Long userId);
 }
